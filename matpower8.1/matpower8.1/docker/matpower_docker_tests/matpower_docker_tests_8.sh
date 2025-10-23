7997e65e16b13046ab92a48580b844d366fc146b
Heictor <39010251+Heictor@users.noreply.github.com>
2025-10-23 20:55:24 -0300

    Files organization

--- matpower8.1/matpower8.1/docker/matpower_docker_tests/matpower_docker_tests_8.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-octave-cli --eval 'mpver'
-octave-cli --eval "test_matpower && test_most && ~have_feature('mp_core', 0) &&  ~have_feature('sdp_pf', 0) && test_matpower && test_mll_main && test_syngrid"
